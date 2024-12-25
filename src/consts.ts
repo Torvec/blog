6570baaccfa8c6f9f69f440fe466e41ac4212d4c
Edward Vonschondorf <edvonschondorf@gmail.com>
2024-12-24 22:22:34 -0800

    Update title for site on index.astro, move the ...slug.astro into the root pages directory so that the urls dont include blog in it since the deployed site starts with blog

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,3 @@
-export const SITE_TITLE = "Blog | Edward Vonschondorf";
+export const SITE_TITLE = "Edward Vonschondorf";
 export const SITE_DESCRIPTION =
-  "A place where I write about my thoughts and experiences.";
+  "A place to share my work and what it took to create it.";
